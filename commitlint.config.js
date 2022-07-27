6d5a673d46317c7da3f647b80ecc5c5533cc0b72
vkm0303 <1947055799@qq.com>
2022-07-27 11:09:15 +0800

    chore: 更新git commit配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['init', 'perf', 'build', 'ci', 'chore', 'merge', 'docs', 'feat', 'fix', 'test'],
+    ],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+  },
 };
